aa4ba4eff7aa1403f4227368774962a888ed4f8f
simondlevy <simon.d.levy@gmail.com>
2020-05-12 15:22:17 -0400

    Begin gym-mygame

---
+++ gym-mygame/gym_mygame/envs/__init__.py
@@ -0,0 +1,1 @@
+from mygame import MyGame
---
+++ gym-mygame/gym_mygame/envs/mygame.py
@@ -0,0 +1,34 @@
+'''
+My OpenAI Gym game environment for CSCI 316 spring 2020
+
+Copyright (C) 2020 A. Student
+
+MIT License
+'''
+
+import gym
+import numpy as np
+
+class MyGame(gym.Env):
+
+    def __init__(self):
+
+        gym.Env.__init__(self)
+
+    def step(self, action):
+
+        return
+
+    def render(self):
+
+        return
+
+    def reset(self):
+
+        return
+
+    def close(self):
+
+        gym.Env.close(self)
+
+        
